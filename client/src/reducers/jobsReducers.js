0537a292f5729d12e64222eb5624c50da6c7d29e
shahid lashari <shahid_lashari001@yahoo.com>
2020-05-24 10:25:45 -0700

    updated reducers and actions

--- client/src/reducers/jobsReducers.js
+++ client/src/reducers/jobsReducers.js
@@ -20,20 +20,20 @@ const INITIAL_STATE = {
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
-    case GET_ALL_JOBS:
-      return {...state, Jobs: action.payload, getAllJobsError: '' };
-    case GET_ALL_JOBS_ERROR:
-      return {...state, getAllJobsError: action.payload };
+    // case GET_ALL_JOBS:
+    //   return {...state, Jobs: action.payload, getAllJobsError: '' };
+    // case GET_ALL_JOBS_ERROR:
+    //   return {...state, getAllJobsError: action.payload };
     case GET_USER_JOBS:
-      return {...state, userJobs: action.payload, getUserJobsClientError: '', getUserJobsServerError: '', updateJobsCompleteError: '', deleteJobsError: '' };
+      return { ...state, userJobs: action.payload, getUserJobsClientError: '', getUserJobsServerError: '', updateJobsCompleteError: '', deleteJobsError: '' };
     case GET_USER_JOBS_ERROR:
-      return {...state, getUserJobsServerError: action.serverError, getUserJobsClientError: action.clientError };
+      return { ...state, getUserJobsServerError: action.serverError, getUserJobsClientError: action.clientError };
     case UPDATE_JOBS_BY_ID_ERROR:
       return { ...state, updateJobsCompleteError: action.payload };
     case ADD_USER_JOBS:
-      return {...state, addJobsError: '' };
+      return { ...state, addJobsError: '' };
     case ADD_USER_JOBS_ERROR:
-      return {...state, addJobsError: action.payload };
+      return { ...state, addJobsError: action.payload };
     case DELETE_JOBS_BY_ID_ERROR:
       return { ...state, deleteJobsError: action.payload };
     default:
