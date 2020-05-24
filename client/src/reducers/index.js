dd38509ba9948376b51c9a650a1d508fbd11b58b
shahid lashari <shahid_lashari001@yahoo.com>
2020-05-24 00:12:21 -0700

    created trendingSchema

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
+import dataReducer from './dataReducers';
+import jobsReducer from './jobsReducers';
 
 // import { ADD_USER_TODO } from '../actions/types';
 
 export default combineReducers({
   auth: authReducer,
+  data: dataReducer,
+  jobs: jobsReducer,
   form: formReducer,
   // .plugin({
   //   addTodo: (state, action) => {
