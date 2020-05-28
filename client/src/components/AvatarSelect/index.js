78b828eb79ce28e39b07ed6436ab6af8ec9c6826
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-28 10:44:33 -0700

    dicebear avatar container and package

---
+++ client/src/components/AvatarSelect/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Field } from 'redux-form';
+import { Image, Segment, Button, Icon } from 'semantic-ui-react';
+
+export default class AvatarSelect extends Component {
+// array of URL strings avatars
+  state={
+    avatars:[
+      `https://avatars.dicebear.com/api/male/example.svg?options[mood][]=happy` ,
+      `https://avatars.dicebear.com/api/female/example.svg?options[mood][]=happy`,
+      `https://avatars.dicebear.com/api/avataaars/example.svg?skin[top][]=shortHair`,
+      ``,
+      ``, 
+    ]
+  }
+    // Redux Forms Display the field
+    // add code to renderSelect Function
+    // give user options to choose from-Avatars
+    renderSelect =() =>{
+
+    }
+
+    render() {
+        return (
+           <Segment>
+               <Field
+                    name="avatar"
+                    component={this.renderSelect}
+
+               />
+           </Segment>
+        )
+    }
+}
