34eb69d40e66becf2eb8f6bd1df9eb5293847474
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-31 20:33:37 -0700

    styled out the search page and cleaned up jobActions and types files

--- client/src/containers/Search/index.js
+++ client/src/containers/Search/index.js
@@ -5,46 +5,45 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import createDOMPurify from 'dompurify';
 import { JSDOM } from 'jsdom';
-
+import { Helmet } from 'react-helmet';
 import {
+  Grid,
   Icon,
   Form,
-  List,
-  Image,
   Modal,
   Header,
   Segment,
-  Container,
   Button,
-  Divider,
-  Dropdown,
   Card,
 } from 'semantic-ui-react';
 import axios from 'axios';
 import {
-  locationOptions,
-  levelOptions,
   jobOptions,
+  levelOptions,
+  locationOptions,
 } from '../../static/location';
 // import levelOptions from '../../static/location';
 // import jobOptions from '../../static/location';
 
 import { SEARCH_JOBS, SEARCH_JOBS_ERROR } from '../../actions/types';
-import { searchJob, searchJobError } from '../../actions/searchActions';
 
 const { window } = new JSDOM('');
 const DOMPurify = createDOMPurify(window);
 
 //  54295
 class Search extends Component {
+  state = {
+    loading: false,
+  }
+
   onSubmit = async (formValues, dispatch) => {
     const { role, level, location } = formValues;
     try {
-      const { data } = await axios.get(
-        `/api/job/search?role=${role}&level=${level}&location=${location}`,
-      );
+      this.setState({ loading: true });
+      const { data } = await axios.get(`/api/job/search?role=${role}&level=${level}&location=${location}`);
       console.log(data);
       dispatch({ type: SEARCH_JOBS, payload: data });
+      this.setState({ loading: false });
     } catch (e) {
       dispatch({ type: SEARCH_JOBS_ERROR, payload: e });
     }
@@ -82,122 +81,108 @@ class Search extends Component {
   render() {
     const { handleSubmit, submitting, submitFailed } = this.props;
     return (
-      <Form
-        style={{ minHeight: 0, padding: '10em 1em', margin: '5em' }}
-        size="large"
-        onSubmit={handleSubmit(this.onSubmit)}
-      >
-        <Segment style={{ padding: '-2em 0em' }} vertical>
-          {/* <Container text>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              Top Industries
+      <div>
+        <Helmet>
+          <style>{'body { background-color: #37373b; }'}</style>
+        </Helmet>
+        <Grid textAlign="center" style={{ height: '450px', padding: '9em 0em' }}>
+          <Grid.Column style={{ maxWidth: 500 }}>
+            <Header color="blue" textAlign="center" style={{ fontSize: '52px' }}>
+              Job Search
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Discover the current top industries for job posts
-            </p>
-            <Button as="a" size="large" inverted color="blue">
-              Find Out More
-            </Button>
+            <Form
+              size="large"
+              onSubmit={handleSubmit(this.onSubmit)}
+            >
+              <Segment vertical style={{ padding: '1em 0em' }}>
+                <Field
+                  name="role"
+                  component={this.renderDropdown}
+                  data={jobOptions}
+                  placeholder="select role"
+                />
+                <Field
+                  name="level"
+                  component={this.renderDropdown}
+                  data={levelOptions}
+                  placeholder="select level"
+                />
+                <Field
+                  name="location"
+                  component={this.renderDropdown}
+                  data={locationOptions}
+                  placeholder="select location"
+                />
+                <Button
+                  loading={this.state.loading}
+                  color="blue"
+                  size="large"
+                  type="submit"
+                  disabled={submitting || submitFailed}
+                >
+                  <Icon name="search" />
+                  Search
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
 
-            <Divider style={{ margin: '2em 0em' }} />
-
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              Top Cities
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Discover the current top cities for job posts
-            </p>
-            <Button as="a" size="large" inverted color="blue">
-              Find Out More
-            </Button>
-            <Divider style={{ margin: '2em 0em' }} />
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              Top Employers
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Discover the current top employers for job posts
-            </p>
-            <Button as="a" size="large" inverted color="blue">
-              Find Out More
-            </Button>
-          </Container> */}
-          <Field
-            name="role"
-            component={this.renderDropdown}
-            data={jobOptions}
-            placeholder="select role"
-          />
-          <Field
-            name="level"
-            component={this.renderDropdown}
-            data={levelOptions}
-            placeholder="select level"
-          />
-          <Field
-            name="location"
-            component={this.renderDropdown}
-            data={locationOptions}
-            placeholder="select location"
-          />
-
-          <Button
-            color="teal"
-            size="large"
-            type="submit"
-            disabled={submitting || submitFailed}
-          >
-            Search
-          </Button>
-
-          <Container>
+        <Grid container centered relaxed stackable textAlign="center">
+          <Grid.Row columns={3} style={{ padding: '1em 0em', marginLeft: '75px' }}>
             { this.props.jobs?.map((job, idx) => {
               return (
-                <Card>
-                  <List.Item key={idx}>
-                    <List.Content>
-                      <List.Header>{job.name}</List.Header>
-                      <List.Header>{job.company.name}</List.Header>
-                      <Button
-                        color="teal"
-                        size="small"
-                        type="submit"
-                        floated="right"
-                        onClick={() => this.saveJob(job)}
+                <Grid.Column stretched textAlign="center" style={{ padding: '1em 0em', maxWidth: 500 }}>
+                  <Card textAlign="center" key={idx}>
+                    <Card.Content as="h1">{job.name}</Card.Content>
+                    <Card.Content>Company: {job.company.name}</Card.Content>
+                    <Card.Content style={{ textAlign: 'center' }}>
+                      <Modal
+                        size="large"
+                        closeIcon
+                        trigger={<Button
+                          color="blue"
+                          size="medium"
+                          type="submit"
+                          floated="right"
+                          style={{ marginRight: '80px' }}
+                        >
+                          View Job
+                        </Button>}
                       >
-                        {' '}
-                        save job
-                      </Button>
-                      <Modal trigger={<Button
-                        color="teal"
-                        size="small"
-                        type="submit"
-                        floated="right"
-                      >
-                        View Job
-                      </Button>}
-                      >
-                        <Modal.Header>
-                          {job.name}
-                          {' '}
-                          {job.locations[0].name}
+                        <Modal.Header style={{ textAlign: 'center' }}>
+                          <p style={{ fontSize: '24px' }}>{job.name}</p>
+                          <p>{job.locations[0].name}</p>
                         </Modal.Header>
 
                         <Modal.Content>
                           <Modal.Description>
-                            <Header>
-                              <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(job.contents) }} />
-                            </Header>
+                            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(job.contents) }} style={{ fontSize: '20px' }} />
                           </Modal.Description>
                         </Modal.Content>
+
+                        <Modal.Actions>
+                          <Button
+                            color="blue"
+                            type="submit"
+                            floated="right"
+                            size="huge"
+                            style={{ marginBottom: '10px' }}
+                            onClick={() => this.saveJob(job)}
+                          >
+                            <Icon name="save" />
+                            Save Job
+                          </Button>
+                        </Modal.Actions>
                       </Modal>
-                    </List.Content>
-                  </List.Item>
-                </Card>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
               );
             })}
-          </Container>
-        </Segment>
-      </Form>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
